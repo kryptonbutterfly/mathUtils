7cd88f3d3fafbbcbcd3d833b56824aa4839c5ae2
tinycodecrank <tinycodecrank@gmail.com>
2023-11-04 10:17:39 +0100

    make IRange a public interface

--- src/de/tinycodecrank/math/utils/range/IRange.java
+++ src/de/tinycodecrank/math/utils/range/IRange.java
@@ -1,6 +1,6 @@
 package de.tinycodecrank.math.utils.range;
 
-interface IRange<T, E extends IRange<T, E>> extends Iterable<T>
+public interface IRange<T, E extends IRange<T, E>> extends Iterable<T>
 {
 	public E reverse();
 }
